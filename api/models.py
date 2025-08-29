5eeff64dc0fe93148c22ac22cdc3ae727936daaa
Evgen <evgeshkarach@gmail.com>
2025-08-29 07:47:51 +0400

    Созданы модели

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=200, verbose_name='Текст вопроса')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания вопроса')
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Вопрос')
+    user_id = models.UUIDField(default=uuid4(), verbose_name='ID пользователя')
+    text = models.TextField(verbose_name='Текст ответа')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания ответа')
